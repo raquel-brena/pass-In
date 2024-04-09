ac0f56cc4f0454fe312ee7383186deac002630e7
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2024-04-09 18:32:30 -0300

    feat: add Checkin class

---
+++ src/main/java/com/rb/passin/domain/checkin/CheckIn.java
@@ -0,0 +1,36 @@
+package com.rb.passin.domain.checkin;
+import java.time.LocalDateTime;
+
+import com.rb.passin.domain.attendee.Attendee;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "check_ins")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CheckIn {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @OneToOne
+    @JoinColumn(name = "attendee_id", nullable = false)
+    private Attendee attendee;
+}
