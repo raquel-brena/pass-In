7697fe5dd1abd58734c3c90a338eb134e002cccd
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2024-04-09 18:32:48 -0300

    feat: add AttendeeController class

---
+++ src/main/java/com/rb/passin/controller/AttendeeController.java
@@ -0,0 +1,10 @@
+package com.rb.passin.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/attendees")
+public class AttendeeController {
+}
