cc7b8dd2aebab36f21b2c95bbc9c97b69b4d34d7
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2024-04-11 14:44:39 -0300

    feat: add EventController class

--- src/main/java/com/rb/passin/controller/EventController.java
+++ src/main/java/com/rb/passin/controller/EventController.java
@@ -1,11 +1,14 @@
 package com.rb.passin.controller;
 
+import com.rb.passin.dto.attendeeDto.AttendeeBadgeResponseDTO;
+import com.rb.passin.dto.attendeeDto.AttendeeRequestDTO;
+import com.rb.passin.dto.attendeeDto.AttendeesListResponseDTO;
 import com.rb.passin.dto.eventDto.EventIdDTO;
 import com.rb.passin.dto.eventDto.EventRequestDTO;
 import com.rb.passin.dto.eventDto.EventResponseDTO;
+import com.rb.passin.service.AttendeeService;
 import com.rb.passin.service.EventService;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -15,19 +18,34 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RequiredArgsConstructor
 public class EventController {
 
-    private final EventService service;
+    private final EventService eventService;
+    private final AttendeeService attendeeService;
 
     @GetMapping("/{id}")
     public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String id) {
-        EventResponseDTO event = this.service.getEventDetail(id);
+        EventResponseDTO event = this.eventService.getEventDetail(id);
         return ResponseEntity.ok(event);
     }
 
     @PostMapping
     public ResponseEntity<EventIdDTO> createEvent (@RequestBody EventRequestDTO event, UriComponentsBuilder uriComponentsBuilder) {
-        EventIdDTO eventId = this.service.createEvent(event);
+        EventIdDTO eventId = this.eventService.createEvent(event);
         var uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventId.id()).toUri();
         return ResponseEntity.created(uri).body(eventId);
     }
+
+    @PostMapping("/{eventId}/attendees")
+    public ResponseEntity<EventIdDTO> registerParticipant (@PathVariable String eventId, @RequestBody AttendeeRequestDTO body, UriComponentsBuilder uriComponentsBuilder) {
+        EventIdDTO eventIdDTO = this.eventService.registerAttendeeOnEvent(eventId, body);
+        var uri = uriComponentsBuilder.path("/events/{id}/badge").buildAndExpand(eventIdDTO.id()).toUri();
+        return ResponseEntity.created(uri).body(eventIdDTO);
+    }
+
+    @GetMapping("/attendees/{eventId}")
+    public ResponseEntity<AttendeesListResponseDTO> getEventAttendees(@PathVariable String eventId) {
+        AttendeesListResponseDTO attendeeList = this.attendeeService.getEventAttendeeList(eventId);
+        return ResponseEntity.ok(attendeeList);
+    }
+
 
 }
