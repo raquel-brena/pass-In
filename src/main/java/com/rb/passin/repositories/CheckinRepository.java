032676f3aebb655c68ccbf454deb3e2f15045eab
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2024-04-11 14:45:25 -0300

    feat: add CheckinRepository class

--- src/main/java/com/rb/passin/repositories/CheckinRepository.java
+++ src/main/java/com/rb/passin/repositories/CheckinRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CheckinRepository extends JpaRepository<CheckIn, Integer> {
+    Optional<CheckIn> findByAttendeeId(String attendeeId);
 }
